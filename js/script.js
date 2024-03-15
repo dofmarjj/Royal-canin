37decfe8f8c0fc7b5443eb0007bf6e170b29009c
kirill <dofmarjj@gmail.com>
2024-03-15 18:37:20 +0200

    add some editss

--- js/script.js
+++ js/script.js
@@ -1,75 +1,10 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const links = document.querySelectorAll("nav a");
-
-//   links.forEach(function (link) {
-//     link.addEventListener("click", function (event) {
-//       event.preventDefault();
-//       const targetId = this.getAttribute("href").substring(1);
-//       const targetBlock = document.getElementById(targetId);
-//       if (targetBlock) {
-//         const offsetTop = targetBlock.offsetTop;
-//         window.scrollTo({
-//           top: offsetTop,
-//           behavior: "smooth",
-//         });
-//       }
-//     });
-//   });
-// });
-
-// $(document).ready(function () {
-//   // Показать/скрыть кнопку "Подняться наверх"
-//   $(window).scroll(function () {
-//     if ($(this).scrollTop() > 1000) {
-//       $("#scrollTopBtn").fadeIn();
-//     } else {
-//       $("#scrollTopBtn").fadeOut();
-//     }
-//   });
-
-//   // Плавная прокрутка к якорю
-//   $("nav a").on("click", function (event) {
-//     if (this.hash !== "") {
-//       event.preventDefault();
-//       const hash = this.hash;
-//       $("html, body").animate(
-//         {
-//           scrollTop: $(hash).offset().top,
-//         },
-//         800,
-//         function () {
-//           window.location.hash = hash;
-//         }
-//       );
-//     }
-//   });
-
-//   // Кнопка "Подняться наверх"
-//   $("#scrollTopBtn").on("click", function (event) {
-//     // $("html, body").animate({ scrollTop: 0 }, "slow");
-//     if (this.hash !== "") {
-//       event.preventDefault();
-//       const hash = this.hash;
-//       $("html, body").animate(
-//         {
-//           scrollTop: $(hash).offset().top,
-//         },
-//         800,
-//         function () {
-//           window.location.hash = hash;
-//         }
-//       );
-//     }
-//   });
-// });
-
 $(document).ready(function () {
   var block2Offset = $("#block3").offset().top;
 
   // Скрываем кнопку "Подняться наверх"
   $("#scrollTopBtn").hide();
 
-  // Показать/скрыть кнопку "Подняться наверх" при достижении второго блока
+  // Показать/скрыть кнопку "Подняться наверх" при достижении блока
   $(window).scroll(function () {
     if ($(this).scrollTop() > block2Offset) {
       $("#scrollTopBtn").fadeIn();
@@ -79,37 +14,22 @@ $(document).ready(function () {
   });
 
   // Плавная прокрутка к якорю с отступом сверху
-  $("nav a").on("click", function (event) {
+  $("nav a, #scrollTopBtn, .cards-block a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
       const hash = this.hash;
-      var targetOffset = $(hash).offset().top - 20; // Уменьшаем отступ сверху
+      var targetOffset = $(hash).offset().top - 64;
       $("html, body").animate(
         {
           scrollTop: targetOffset,
         },
-        800,
+        2000,
         function () {
           window.location.hash = hash;
           // Останавливаем прокрутку
           $("html, body").stop();
         }
       );
-    }
-  });
-
-  // Кнопка "Подняться наверх"
-  $("#scrollTopBtn").on("click", function () {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-  });
-
-  // Управление прокруткой стрелочками клавиатуры
-  $(document).keydown(function (e) {
-    if (e.keyCode == 37 || e.keyCode == 39) {
-      // Стрелка влево или вправо
-      e.preventDefault();
-      var delta = e.keyCode == 37 ? -120 : 120; // Определяем направление прокрутки
-      $(window).scrollLeft($(window).scrollLeft() + delta); // Прокручиваем страницу на определенное количество пикселей
     }
   });
 });
