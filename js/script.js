d439e73c0e6c821297ca8b47164a389a46f9aea1
kirill <dofmarjj@gmail.com>
2024-03-18 15:07:47 +0200

    fin_1

--- js/script.js
+++ js/script.js
@@ -59,3 +59,16 @@ $(document).ready(function () {
     $(this).addClass("active__btn");
   });
 });
+
+// Full
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var elems = document.querySelectorAll(".materialboxed");
+//   var instances = M.Materialbox.init(elems, options);
+// });
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
+});
