4daa632a47aa25a1e0d571b67ced9dd4891271d7
kirill <dofmarjj@gmail.com>
2024-03-20 13:14:50 +0200

    add fixs 1

--- scroll.js
+++ scroll.js
@@ -1,14 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var slider = document.getElementById("slider");
-  var scrollWrapper = document.getElementById("scrollWrapper");
+$(document).ready(function () {
+  var $slider = $("#slider");
+  var $scrollWrapper = $("#scrollWrapper");
 
-  slider.addEventListener("input", function () {
-    var value =
-      (slider.value * (scrollWrapper.scrollWidth - scrollWrapper.clientWidth)) /
+  // Функция обновления ползунка в соответствии с текущим положением прокрутки
+  function updateSliderPosition() {
+    var scrollPercentage =
+      ($scrollWrapper.scrollLeft() /
+        ($scrollWrapper[0].scrollWidth - $scrollWrapper[0].clientWidth)) *
       100;
-    scrollWrapper.scrollLeft = value;
+    $slider.val(scrollPercentage);
+    var gradientValue = (scrollPercentage * 100) / $slider.attr("max");
+    $slider.css(
+      "background",
+      "linear-gradient(to right, #e2001a 0%, #e2001a " +
+        gradientValue +
+        "%, #d3d3d3 " +
+        gradientValue +
+        "%, #d3d3d3 100%)"
+    );
+  }
 
-    var gradientValue = (slider.value * 100) / slider.max;
-    slider.style.background = `linear-gradient(to right, #e2001a 0%, #e2001a ${gradientValue}%, #d3d3d3 ${gradientValue}%, #d3d3d3 100%)`;
+  // Обработчик события прокрутки контейнера
+  $scrollWrapper.on("scroll", function () {
+    updateSliderPosition();
+  });
+
+  // Обновляем ползунок при изменении его значения
+  $slider.on("input", function () {
+    var value =
+      ($slider.val() *
+        ($scrollWrapper[0].scrollWidth - $scrollWrapper[0].clientWidth)) /
+      100;
+    $scrollWrapper.scrollLeft(value);
+    updateSliderPosition();
   });
 });
