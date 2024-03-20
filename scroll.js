a07654d9544fae735995c401e6ed41a8827115ad
kirill <dofmarjj@gmail.com>
2024-03-20 11:35:32 +0200

    add fixs

---
+++ scroll.js
@@ -0,0 +1,12 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var slider = document.getElementById("slider");
+//   var image = document.getElementById("image");
+
+//   slider.addEventListener("input", function () {
+//     var value =
+//       (parseInt(slider.value) *
+//         (image.offsetWidth - image.parentElement.offsetWidth)) /
+//       100;
+//     image.style.transform = "translateX(-" + value + "px)";
+//   });
+// });
