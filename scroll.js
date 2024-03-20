92fd17c1049cde0a14c34acc9d06e6a1005c1b78
kirill <dofmarjj@gmail.com>
2024-03-20 12:42:24 +0200

    test-1

--- scroll.js
+++ scroll.js
@@ -7,5 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
       (slider.value * (scrollWrapper.scrollWidth - scrollWrapper.clientWidth)) /
       100;
     scrollWrapper.scrollLeft = value;
+
+    var gradientValue = (slider.value * 100) / slider.max;
+    slider.style.background = `linear-gradient(to right, #e2001a 0%, #e2001a ${gradientValue}%, #d3d3d3 ${gradientValue}%, #d3d3d3 100%)`;
   });
 });
